1982faaa9570c34c5ef4e97103edfdf37dd98096
Aditi Upadhyay <au.aditi08@gmail.com>
2025-07-17 13:31:57 +0530

    add navbar component

--- music-academy-project/src/app/page.tsx
+++ music-academy-project/src/app/page.tsx
@@ -1,6 +1,8 @@
 
 export default function Home() {
   return (
-    <h1 className="text-2xl text-center">Hello World!</h1>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+       <h1 className="text-2xl text-center">Hello World!</h1>
+    </main>
   );
 }
